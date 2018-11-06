1190e3837e761284e2d4609b26600b1727853ab2
Quinn Slack <sqs@sourcegraph.com>
2018-11-06 04:43:02 -0800

    docs: update generated docs to use new extensions docs links

--- src/cli.ts
+++ src/cli.ts
@@ -219,7 +219,7 @@ async function main(): Promise<void> {
             '',
             '- **Creation**: Install [Node.js](https://nodejs.org).',
             '- **Publishing**: Install the [Sourcegraph CLI (`src`)](https://github.com/sourcegraph/src-cli#installation) and create a [Sourcegraph.com account](https://sourcegraph.com/sign-up).',
-            '- **Viewing**: Install the Sourcegraph extension for [Chrome](https://chrome.google.com/webstore/detail/sourcegraph/dgjhfomjieaadpoljlnidmbgkdffpack) or [Firefox](https://addons.mozilla.org/en-US/firefox/addon/sourcegraph/).',
+            '- **Viewing**: Install the [Sourcegraph browser extension](https://docs.sourcegraph.com/integration/browser_extension).',
             '',
             '## Set up',
             '',
@@ -242,7 +242,7 @@ async function main(): Promise<void> {
             '',
             '## Sourcegraph extension API',
             '',
-            'Visit the [Sourcegraph extension documentation](https://github.com/sourcegraph/sourcegraph-extension-docs) and check out some [Sourcegraph extension samples](https://github.com/sourcegraph/sourcegraph-extension-samples).',
+            'Visit [Sourcegraph extensions documentation](https://docs.sourcegraph.com/extensions) and check out some [sample Sourcegraph extensions](https://github.com/sourcegraph/sourcegraph-extension-samples).',
             '',
         ].join('\n')
         await writeFile('README.md', readme)
